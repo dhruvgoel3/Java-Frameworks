1470d974716ae30637c262e606c9ff24671b8455
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-09-14 23:19:23 +0530

    project

---
+++ song player/src/Song.java
@@ -0,0 +1,37 @@
+public class Song
+{
+    String title;
+    double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+    public Song()
+    {
+
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
